c9cdf886b94c4e05418106820224b8043d8f573f
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-03-27 13:37:33 -0400

    add load function to prevent running the script from function and import in the same function call

--- src/scripts/refreshCommands.ts
+++ src/scripts/refreshCommands.ts
@@ -1,9 +1,14 @@
 import * as dotenv from "dotenv";
 import CommandHandler from "../lib/handlers/CommandHandler";
 
+// NOTE: See scripts/keepAlive.ts for layout info
+
 export function refreshCommands() {
     dotenv.config();
     CommandHandler.loadSlashCommandFolder("dist/commands")
     CommandHandler.refreshSlashCommandRegistry(process.env.CLIENT_LOGIN_TOKEN)
 }
+
+export function loadRefreshCommands(){}
+
 refreshCommands()
