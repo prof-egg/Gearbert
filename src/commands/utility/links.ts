263a101ed8d2be5c7485ecf9e26fbb76660c6e96
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-04-07 19:00:14 -0400

    changed to work with auto complete suggestions

--- src/commands/utility/links.ts
+++ src/commands/utility/links.ts
@@ -1,26 +1,40 @@
 import Discord from "discord.js"
 import Util from "../../lib/util/Util.js";
-import { ECommandTags, ISlashCommandFunc } from "../../lib/handlers/CommandHandler.js";
+import { ECommandTags, ISlashCommandAutocompleteFunc, ISlashCommandFunc } from "../../lib/handlers/CommandHandler.js";
 import messageconfig from "../../config/messages.json" assert { type: 'json' }
+
+const queryOption = "query"
 
 const commandFunction: ISlashCommandFunc = async (interaction, options, client, loggerID) => {
 
-    const title = "Links"
-    let msg = ""
-    messageconfig.links.forEach((linkSet) => {
-        msg += `${linkSet[0]}: ${linkSet[1]}\n`
-    })
+    const query = options.getString(queryOption)
+    if (!query) 
+        return interaction.reply({ embeds: [Util.embedMessage(`Missing ${queryOption} option`)] })
+    
+    const link = messageconfig.links.find((link) => query == link.name) ?? { name: "Bad Query", value: messageconfig.error.query.badLink}
+    const msg = `**${link.name}:** ${link.value}`
+    interaction.reply({ embeds: [Util.embedMessage(msg)] })
+}
 
-    const embed = Util.standardEmbedMessage(title, msg)
-
-    interaction.reply({ embeds: [embed] })
+const autocomplete: ISlashCommandAutocompleteFunc = async (interaction, options, client, loggerID) => {
+    const focusedValue = options.getFocused();
+    const choices = messageconfig.links.map((link) => link.name)
+    const filtered = choices.filter(choice => choice.toLowerCase().startsWith(focusedValue.toLowerCase()));
+    await interaction.respond(
+        filtered.map(choice => ({ name: choice, value: choice })),
+    );
 }
 
 const buildData = new Discord.SlashCommandBuilder()
     .setName("links")
     .setDescription("Returns some links related to Big Boy Games")
+    .addStringOption(option =>
+        option.setName(queryOption)
+            .setDescription('Link to search for')
+            .setAutocomplete(true)
+            .setRequired(true))
     .toJSON()
 
 const tags: ECommandTags[] = [ECommandTags.Complete, ECommandTags.Utility]
 
-export { commandFunction, buildData, tags }
+export { commandFunction, autocomplete, buildData, tags }
