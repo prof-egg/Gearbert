f3225e1c2419e3595aeaf0a7632ce2c2cb524062
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-04-07 14:27:25 -0400

    changed refresh script function to return a promise

--- src/scripts/refreshCommands.ts
+++ src/scripts/refreshCommands.ts
@@ -3,10 +3,13 @@ import CommandHandler from "../lib/handlers/CommandHandler.js";
 
 // NOTE: See scripts/keepAlive.ts for layout info
 
-export function refreshCommands() {
-    dotenv.config();
-    CommandHandler.loadSlashCommandFolder("dist/commands")
-    CommandHandler.refreshSlashCommandRegistry(process.env.CLIENT_LOGIN_TOKEN)
+export function refreshCommands(): Promise<void> {
+    return new Promise(async (resolve) => {
+        dotenv.config();
+        await CommandHandler.loadSlashCommandFolder("dist/commands")
+        await CommandHandler.refreshSlashCommandRegistry(process.env.CLIENT_LOGIN_TOKEN)
+        resolve()
+    })
 }
 
 export function loadRefreshCommands(){}
