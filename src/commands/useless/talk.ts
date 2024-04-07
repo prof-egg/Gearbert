9f741a1323d2f73b2d6dfe3a2e68aa6dad405073
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-04-07 14:26:52 -0400

    add new talk command

---
+++ src/commands/useless/talk.ts
@@ -0,0 +1,23 @@
+import Discord from "discord.js"
+import { ECommandTags, ISlashCommandFunc } from "../../lib/handlers/CommandHandler.js";
+import clientconfig from "../../config/client.json" assert { type: 'json' }
+
+const inputOption = "input"
+
+const commandFunction: ISlashCommandFunc = async (interaction, options, client, loggerID) => {
+    interaction.reply({ content: "I have to reply to this because discord said so ¯\\_(ツ)_/¯", ephemeral: true })
+    interaction.channel?.send({ content: options.getString(inputOption) ?? "I forgot what I'm supposed to say" })
+}
+
+const buildData = new Discord.SlashCommandBuilder()
+    .setName("talk")
+    .setDescription(`Silly command to make ${clientconfig.name} talk`)
+    .addStringOption(option =>
+		option.setName(inputOption)
+			.setDescription('The input to echo back')
+            .setRequired(true))
+    .toJSON()
+
+const tags: ECommandTags[] = [ECommandTags.Incomplete, ECommandTags.Utility]
+
+export { commandFunction, buildData, tags }
