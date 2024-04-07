04cb4ea6c0b2540b32817983136d6ca3deae378f
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-04-07 18:59:54 -0400

    added support for autocomplete suggestions on slash commands

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -4,18 +4,25 @@ import { IEventFunc } from "../lib/handlers/EventHandler.js"
 import Util from "../lib/util/Util.js"
 import messageConfig from "../config/messages.json" assert { type: 'json' }
 import clientconfig from "../config/client.json" assert { type: 'json' }
+import Debug from "../lib/util/Debug.js"
 
 const eventType = Events.InteractionCreate
 
-const eventFunction: IEventFunc<typeof eventType> = async (client, loggerID, interaction) => {
+const processCommand: IEventFunc<typeof eventType> = async (client, loggerID, interaction) => {
 
-    // Filter interaction commands
-    if (!interaction.isCommand()) return
+    // For typesafety
     interaction = interaction as Discord.ChatInputCommandInteraction
+    
+    // Get command
+    const slashCommand = CommandHandler.getSlashCommand(interaction)
+    if (!slashCommand) {
+        Debug.logError(messageConfig.error.command.processedNonExistentCommand, loggerID)
+        console.log(interaction)
+        return
+    }
 
     // If command is marked incomplete then check for bot testing role to execute command
-    const slashCommand = CommandHandler.getSlashCommand(interaction)
-    if (slashCommand?.hasTag(ECommandTags.Incomplete) && interaction.member instanceof Discord.GuildMember){
+    if (slashCommand.hasTag(ECommandTags.Incomplete) && interaction.member instanceof Discord.GuildMember){
 
         const isBotTester = interaction.member.roles.cache.has(clientconfig.homeGuild.roles.botTesterId)
         
@@ -28,6 +35,33 @@ const eventFunction: IEventFunc<typeof eventType> = async (client, loggerID, int
     
     // Execute command
     CommandHandler.executeSlashCommand(interaction)
+}
+
+const processAutocomplete: IEventFunc<typeof eventType> = async (client, loggerID, interaction) => {
+
+    // For typesafety
+    interaction = interaction as Discord.AutocompleteInteraction
+
+    // Get command
+    const slashCommand = CommandHandler.getSlashCommand(interaction)
+    if (!slashCommand) {
+        Debug.logError(messageConfig.error.command.processedNonExistentCommand, loggerID)
+        console.log(interaction)
+        return
+    }
+
+    if(slashCommand.hasAutocomplete()) { slashCommand.autocomplete(interaction, client) }
+}
+
+const eventFunction: IEventFunc<typeof eventType> = async (client, loggerID, interaction) => {
+
+    // Filter command interactions
+    if (interaction.isCommand()) 
+        processCommand(client, loggerID, interaction)
+
+    // Filter autocomplete interactions
+    if (interaction.isAutocomplete()) 
+        processAutocomplete(client, loggerID, interaction)
 }
 
 const eventData = {
